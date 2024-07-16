86a43e79dc219fdb2ca4f760508d4d8c082e3e5e
Ange Arsene <nkenmandenga@gmail.com>
2024-07-16 11:29:21 +0100

    fixe typo

--- public/index.php
+++ public/index.php
@@ -5,7 +5,6 @@ use application\controllers\HomeController;
 use application\middlewares\AuthenticationMiddleware;
 
 // Define the root directory of the application
-// This comment explains the purpose of the constant definition
 define('ROOT_DIR', dirname(__DIR__));
 
 // Include the Composer autoloader to load the application classes
@@ -16,12 +15,10 @@ require_once ROOT_DIR . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . '
 $application = new Application();
 
 // Add the authentication middleware to the list of middlewares
-// This comment explains the purpose of adding the middleware
 $application->middlewares->add('auth', AuthenticationMiddleware::class);
 
 // Register a GET route for the root path ('/') that maps to the 'index' action
 // of the 'HomeController'
-// This comment explains the purpose of the route registration
 $application->router->get('/', [HomeController::class, 'index']);
 
 // Run the application
