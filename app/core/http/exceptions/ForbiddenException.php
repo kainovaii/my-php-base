6392b2ceab25136523a31e3355145712108683f7
Ange Arsene <nkenmandenga@gmail.com>
2024-07-17 01:52:04 +0100

    comment

--- app/core/http/exceptions/ForbiddenException.php
+++ app/core/http/exceptions/ForbiddenException.php
@@ -6,9 +6,15 @@ namespace application\core\http\exceptions;
 
 use application\core\exceptions\HTTPException;
 
+/**
+ * Represents an HTTP 403 Forbidden exception.
+ *
+ * This exception is thrown when the user is not authorized to access the requested resource.
+ * It extends the `HTTPException` class and sets the appropriate HTTP status code and error message.
+ */
 final class ForbiddenException extends HTTPException 
 {
-        /**
+    /**
      * The HTTP status code for this exception.
      *
      * @var int
